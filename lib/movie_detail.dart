356235a0f80dd1acfdd4fe4cd9eb49947b3b9748
kang <kgp294d@gmail.com>
2023-06-07 18:38:21 +0900

    6/7

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:readmore/readmore.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MovieDetail extends StatelessWidget {
   Map<String, dynamic> movie;
@@ -11,7 +13,25 @@ class MovieDetail extends StatelessWidget {
         .toString()
         .replaceAll(' !HS ', '')
         .replaceAll(' !HE ', '');
-    var posters = movie['posters'].split('|')[0];
+    dynamic titleImage = movie['posters'].toString().isEmpty
+        ? Image.asset('asset/images/no-image.png')
+        : Image.network(movie['posters'].toString().split('|')[0]);
+    // var stills = movie['stlls'].toString().split('|');
+    List<Widget> stills = [];
+    if (movie['stlls'].toString().isEmpty) {
+      stills.add(Image.asset(
+        'asset/images/no-image.png',
+        fit: BoxFit.fitHeight,
+      ));
+    } else {
+      for (var k in movie['stills'].toString().split('|')) {
+        stills.add(Image.network(
+          k,
+          fit: BoxFit.fitHeight,
+        ));
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('영화상세 정보(${'movieTitle'})')),
       body: SingleChildScrollView(
@@ -21,13 +41,13 @@ class MovieDetail extends StatelessWidget {
             Row(
               children: [
                 Text(movie['keywords']),
-                Hero(tag: movie['movieSeq'], child: Image.network(posters)),
+                Hero(tag: movie['movieSeq'], child: titleImage),
               ],
             ),
             ReadMoreText(
               movie['plots']['plot'][0]['plotText'],
               colorClickableText: Colors.red,
-              trimLines: 2,
+              trimLines: 2000,
               trimMode: TrimMode.Line,
               trimCollapsedText: '더보기',
               trimExpandedText: '숨기기',
@@ -37,7 +57,18 @@ class MovieDetail extends StatelessWidget {
                   color: Colors.blue),
               moreStyle: const TextStyle(
                   fontSize: 14, fontWeight: FontWeight.bold, color: Colors.red),
-            )
+            ),
+            ImageSlideshow(
+              autoPlayInterval: 2,
+              width: double.infinity,
+              height: 200,
+              children: stills,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  launchUrl(Uri.parse(movie['kmdbUrl'].toString()));
+                },
+                child: const Text('사이트 연결'))
           ],
         ),
       ),
